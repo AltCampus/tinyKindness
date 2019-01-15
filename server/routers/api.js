eddfdaf8040bcaa338d022d38c0d9a8a2bf9e931
kml1326 <kmlraj131996@gmail.com>
2019-01-15 22:56:17 +0530

    provide jwt token

--- server/routers/api.js
+++ server/routers/api.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 const helperController = require('../controllers/helper.controller');
+
+const myJWTSecretKey = 'tinyKindness';
 
 router.get('/check', (req, res) => {
   res.send('You are connected to TinyKindness');
@@ -8,5 +12,20 @@ router.get('/check', (req, res) => {
 router.post('/helper', helperController.addHelper);
 
 router.get('/helper', helperController.displayAllHelper);
+
+router.get('/user/:token', (req, res) => {
+  console.log(req.params.token, 'token in verify');
+  try {
+    const tokenDecodedData = jwt.verify(req.params.token, myJWTSecretKey);
+    User.findById(tokenDecodedData.id, (err, data) => {
+      if (!err) return res.json(data);
+    });
+  } catch (error) {
+    return res.json({
+      error: true,
+      data: error,
+    });
+  }
+});
 
 module.exports = router;
