766c70a40e31cabab68936dab799e01939b8499b
klassynihal <harshaan.n.khan@gmail.com>
2019-02-20 12:02:43 +0530

    set to controllers

--- server/routers/api.js
+++ server/routers/api.js
@@ -1,9 +1,9 @@
 // import modules
 const router = require("express").Router();
 
-const IntroductionTag = require('../models/IntroductionTag');
-const FeedbackTag = require('../models/FeedbackTag');
-const ResourcesTag = require('../models/ResourcesTag');
+const introductionsController = require("../controllers/introductionTag.controller")
+const feedbackController = require("../controllers/feedbackTag.controller")
+const resourcesController = require("../controllers/resourcesTag.controller")
 
 
 
@@ -11,47 +11,11 @@ router.get("/check", (req, res) => {
   res.send("You are connected to TinyKindness");
 });
 
-router.get("/v1/introductions", (req, res) => {
-  if (!req.query.q) {
-    IntroductionTag.find({}, (err, data) => {
-      if (!err) res.send(data);
-    });
-  } else{
-    const query = req.query.q;
-    const regexp = new RegExp(query);
-    IntroductionTag.find({ name: { $regex: regexp } }, (err, data) => {
-      if (!err) res.send(data);
-    });
-  }
-});
+router.get("/v1/introductions",introductionsController.introductions );
 
-router.get("/v1/feedback", (req, res) => {
-  if (!req.query.q) {
-    FeedbackTag.find({}, (err, data) => {
-      if (!err) res.send(data);
-    });
-  } else {
-    const query = req.query.q;
-    const regexp = new RegExp(query);
-    FeedbackTag.find({ name: { $regex: regexp } }, (err, data) => {
-      if (!err) res.send(data);
-    });
-  }
-});
+router.get("/v1/feedback",feedbackController.feedbacks );
 
-router.get("/v1/resources", (req, res) => {
-  if (!req.query.q) {
-    ResourcesTag.find({}, (err, data) => {
-      if (!err) res.send(data);
-    });
-  } else {
-    const query = req.query.q;
-    const regexp = new RegExp(query);
-    ResourcesTag.find({ name: { $regex: regexp } }, (err, data) => {
-      if (!err) res.send(data);
-    });
-  }
-});
+router.get("/v1/resources",resourcesController.resources);
 
 // verify user by JWT token and send user data
 // export router
