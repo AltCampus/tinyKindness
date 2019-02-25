906331ed9f5a6bfa3067284a44419a56c30f9f56
ashwanig3 <goswami17829@gmail.com>
2019-02-25 12:29:46 +0530

    post userdata from frontend and add helperlist component

--- client/src/store/actions/actionCreator.js
+++ client/src/store/actions/actionCreator.js
@@ -1,34 +1,49 @@
 const url = "http://localhost:8001/api/v1";
 
 export function getIntroductionTag(query, cb) {
-	return (dispatch) => {
-		fetch(`${url}/introductions?q=${query}`)
-			.then((res) => res.json())
-			.then((data) => {
-				dispatch({ type: "INTRODUCTION_TAGS", data });
-				cb(true, data);
-			});
-	};
+  return dispatch => {
+    fetch(`${url}/introductions?q=${query}`)
+      .then(res => res.json())
+      .then(data => {
+        dispatch({ type: "INTRODUCTION_TAGS", data });
+        cb(true, data);
+      });
+  };
 }
 
 export function getResourceTags(query, cb) {
-	return (dispatch) => {
-		fetch(`${url}/resources?q=${query}`)
-			.then((res) => res.json())
-			.then((data) => {
-				dispatch({ type: "RESOURCES_TAGS", data });
-				cb(true, data);
-			});
-	};
+  return dispatch => {
+    fetch(`${url}/resources?q=${query}`)
+      .then(res => res.json())
+      .then(data => {
+        dispatch({ type: "RESOURCES_TAGS", data });
+        cb(true, data);
+      });
+  };
 }
 
 export function getFeedbackTags(query, cb) {
-	return (dispatch) => {
-		fetch(`${url}/feedback?q=${query}`)
-			.then((res) => res.json())
-			.then((data) => {
-				dispatch({ type: "FEEDBACK_TAGS", data });
-				cb(true, data);
-			});
-	};
+  return dispatch => {
+    fetch(`${url}/feedback?q=${query}`)
+      .then(res => res.json())
+      .then(data => {
+        dispatch({ type: "FEEDBACK_TAGS", data });
+        cb(true, data);
+      });
+  };
+}
+
+export function submitUserData(data, userId) {
+  console.log(data, userId);
+  return dispatch => {
+    fetch(`${url}/${userId}/info`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data));
+  };
 }
