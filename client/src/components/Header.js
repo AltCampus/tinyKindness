0ab8205de04a08fe00ed8ce47f71186ff2f0e37b
ashish4497 <ak8393267@gmail.com>
2019-02-25 15:51:23 +0530

    add search button in header seaction

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,23 +1,51 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import SignedInLink from "./SignedInLink";
 
 class Header extends Component {
-  constructor(props) {
-    super(props)
-  }
-  
-  render() {
-    return (
-      <header>
-        <div className="wrapper header-wrapper">
-          <Link to="/" className="logo">
-            tinyKindness
-          </Link>
-        </div>
-      </header>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			query: ""
+		};
+	}
+
+	handleChange = (e) => {
+		this.setState({
+			query: e.target.value
+		});
+	};
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.props.dispatch(searchUserByTag(this.state.query));
+	};
+	render() {
+		return (
+			<div className='header-seaction'>
+				<header className='top_header'>
+					<div className='wrapper header-wrapper'>
+						<Link to='/' className='logo'>
+							tinyKindness
+						</Link>
+					</div>
+					<div className='search-input'>
+						<form onSubmit={this.handleSubmit}>
+							<input
+								type='text'
+								placeholder='search'
+								className='search'
+								onChange={this.handleChange}
+							/>
+						</form>
+						<div className='signIn-Link'>
+							<SignedInLink />
+						</div>
+					</div>
+				</header>
+			</div>
+		);
+	}
 }
 
-export default Header;
+export default connect()(Header);
